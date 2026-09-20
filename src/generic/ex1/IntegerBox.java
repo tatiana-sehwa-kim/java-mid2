6c878a23a011ce7cf4685a088e4bada362ff615b
tatiana-sehwa-kim <kseha1997@gmail.com>
2026-09-20 23:58:11 +0900

    섹션2. 제네릭Generic1- 제네릭이 필요한 이유

---
+++ src/generic/ex1/IntegerBox.java
@@ -0,0 +1,14 @@
+package generic.ex1;
+
+public class IntegerBox {
+
+    private Integer value;      // 숫자를 보관하는 박스
+
+    public void set(Integer value) {
+        this.value = value;
+    }
+
+    public Integer get() {
+        return value;
+    }
+}
