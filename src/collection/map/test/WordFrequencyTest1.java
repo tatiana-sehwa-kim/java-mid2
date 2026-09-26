685dfca3a9709828197e50e4f045b358b3f266b6
tatiana-sehwa-kim <kseha1997@gmail.com>
2026-09-26 14:29:52 +0900

    Map의 누적 카운트: count 카운트 추출 후 null이면 0 주고 count ++, map.put();

--- src/collection/map/test/WordFrequencyTest1.java
+++ src/collection/map/test/WordFrequencyTest1.java
@@ -9,13 +9,18 @@ public class WordFrequencyTest1 {
 
         String text = "orange banana apple apple banana apple";
 
-        String[] fruits = text.split(" ");
-        Map<String, Integer> map = new HashMap<>();
+        String[] fruits = text.split(" ");          // 스플릿 후 배열화
+        Map<String, Integer> map = new HashMap<>();         // 맵 생성
 
         for (String fruit : fruits) {
-            map.put(fruit, map.getOrDefault(fruit, 0) + 1);   // 장부에서 fruit을 찾고 이미 적혀 있으면 원래 적혀있던 숫자 꺼내주고
-        }                                                     // 처음 보는 단어라서 아무것도 없으면(null이면) 당황하지 말고 그냥 0으로 줘!"
-                                                              // 이미 있는거면 원래 값 + 1 으로 덮어쓰기
+            Integer count = map.get(fruit);         // 원래 있었나 확인 밸류가 몇인가
+            if (count == null) {                    // 없으면
+                count = 0;                          // 0으로 시작
+            }
+            count ++;                               // 한번 카운트
+            map.put(fruit, count);                  // 과일이름(키)와 카운트
+        }
+
         System.out.println(map);
     }
 }
